150af766ea4e6fecdfa16262241db0467c0c8247
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-04 17:06:48 +0100

    first header layout

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -2,13 +2,27 @@ import dcLogo from '../assets/img/dc-Logo.png'
 
 export default function Header() {
     return (
-        <header>
-            <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
+        <header className='bg-light'>
+            <nav className="container navbar d-flex justify-content-between">
+                <div>
                     <a className="navbar-brand" href="#">
-                        <img src={dcLogo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                        <img src={dcLogo} alt="Logo" width="30" height="24" />
                     </a>
                 </div>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
